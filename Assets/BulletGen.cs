e3507df4d22db0a9421980add097fade2949f820
Santiago <santi.volcanos@gmail.com>
2023-05-31 08:51:11 -0600

    Bulletgen

--- Assets/BulletGen.cs
+++ Assets/BulletGen.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class BulletGen : MonoBehaviour
 {
     [SerializeField] private int samples;
+    [SerializeField] private float horizontalSize;
+    [SerializeField] private float verticalSize;
+    [SerializeField] private float Circunference;
+    [SerializeField] private float offset;
+    [SerializeField] private float force;
+    [SerializeField] private GameObject bulletPrefab;
     private void Awake()
     {
         Vector2[] positions = Generate();
+        for (int i = 0; i < positions.Length; i++)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, positions[i], Quaternion.identity);
+            Vector2 Direction = positions[i] - (Vector2)transform.position;
+            bullet.GetComponent<Rigidbody2D>().AddForce(Direction.normalized * force);
+        }
         for (int i = 0; i < positions.Length-1; i++)
         {
             Vector2 o = positions[i];
@@ -18,14 +30,16 @@ public class BulletGen : MonoBehaviour
     public Vector2[] Generate()
     {
         Vector2[] result = new Vector2[samples];
-        float angle = (Mathf.PI * 2) / samples;
+        
+        float angle = Circunference / samples;
         for (int i = 0; i < samples; i++)
         {
             float a = i * angle;
 
             Vector2 pos = new Vector2();
-            pos.x = Mathf.Sin(a);
-            pos.y =Mathf.Cos(a);
+            pos.x = Mathf.Sin(offset+a)*horizontalSize;
+            pos.y =Mathf.Cos(offset+a)*verticalSize;
+            pos += (Vector2)transform.position;
             result[i] = pos;
         }
         return result;
